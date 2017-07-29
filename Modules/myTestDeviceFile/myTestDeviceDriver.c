6dd98e98ef98a45d8883977e1594ed2717c169d1
hark130 <makleford@Peke-Ito.localdomain>
2017-07-29 11:58:25 -0700

    MASTER: New LKM includes setup
    Incremental commit. -Hark

--- Modules/myTestDeviceFile/myTestDeviceDriver.c
+++ Modules/myTestDeviceFile/myTestDeviceDriver.c
@@ -1,6 +1,10 @@
 #include <linux/module.h>       // ALWAYS NEED
 #include <linux/kernel.h>       // ALWAYS NEED
+// Used to allow file operations
 #include <linux/fs.h>           // file_operations structure - allows use to open/close, read/write to device
+// Used to help register your char device to the kernel
 #include <linux/cdev.h>         // This is a char driver, makes cdev available
+// Multi-threading synchronization
 #include <linux/semaphore.h>    // used to access semaphores, synchronization behaviors
+// Used to assist in mapping data from user space to kernel space
 #include <asm/uacess.h>         // copy_to_user, copy_from_user

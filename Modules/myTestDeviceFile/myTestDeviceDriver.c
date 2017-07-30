f54bbb053a8099403df4f9e1a1b883a6dc2c8d11
hark130 <hark130@yahoo.com>
2017-07-29 17:15:47 -0700

    MASTER: Driver works(?)
    Forced reboot because it's not doing anything with the input.  See you on the other side. -Hark

--- Modules/myTestDeviceFile/myTestDeviceDriver.c
+++ Modules/myTestDeviceFile/myTestDeviceDriver.c
@@ -9,8 +9,9 @@
 #include <linux/cdev.h>         // This is a char driver, makes cdev available
 // Multi-threading synchronization
 #include <linux/semaphore.h>    // used to access semaphores, synchronization behaviors
+// PRO TIP:  Only for Solaris --> #include <linux/synch.h>          // USYNC_PROCESS
 // Used to assist in mapping data from user space to kernel space
-#include <asm/uacess.h>         // copy_to_user, copy_from_user
+#include <asm/uaccess.h>         // copy_to_user, copy_from_user
 
 
 // 1. Create a struct for our fake device
@@ -29,7 +30,76 @@ dev_t dev_num;          // Will hold major number that kernel gives us
                         //  name--> appears in /proc/devices
 #define DEVICE_NAME     "myTestDevice"
 
+// 7. Define the file operation functions
+// 7.1. device_open
+//  Called on defice_file open
+//      inode reference to the file on disk
+//      and contains information about that file
+//      struct file represents an abstract open file
+int device_open(struct inode *inode, struct file *filp)
+{
+    // Only allow one process to open this device by using a semaphore as mutal exclusive lock - mutext
+    retVal = down_interruptible(&virtual_device.sem);
+    if (retVal != 0)
+    {
+        printk(KERN_ALERT "myTestDevice: could not lock device during open\n");
+        return -1;
+    }
+    else
+    {
+        printk(KERN_INFO "myTestDevice: opened device\n");
+    }
 
+    return 0;
+}
+
+
+// 7.2. device_read
+// Called when user wants to get information from the device
+ssize_t device_read(struct file* filp, char* bufStoreData, size_t bufCount, loff_t* curOffset)
+{
+    // Take data from kernel space (device) to user space (process)
+    printk(KERN_INFO "myTestDevice: reading from device\n");
+    return copy_to_user(bufStoreData, virtual_device.data, bufCount);
+}
+
+
+// 7.3. device_write
+// Called when user wants to send information to the device
+ssize_t device_write(struct file* filp, const char* bufSourceData, size_t bufCount, loff_t* curOffset)
+{
+    // Send data from user to kernel
+    // copy_from_user(dest, source, count)
+    printk(KERN_INFO "myTestDevice: writing to device\n");
+    return copy_from_user(virtual_device.data, bufSourceData, bufCount);
+}
+
+
+// 7.4. device_close
+// Called upon user close
+int device_close(struct inode *inode, struct file *filp)
+{
+    // By calling up, which is opposite of down for semaphores, we release
+    //  the mutex that we obtained at device open
+    // This has the effect of allowing other processes to use the device now
+    up(&virtual_device.sem);
+    printk(KERN_INFO "myTestDevice: closed device\n");
+    return 0;
+}
+
+
+// 6. Define the file_operations struct
+// This tells the kernel which functions to call when a user operates on our device file_operations
+struct file_operations fops =
+{
+    .owner = THIS_MODULE,       // Prevent unloading of this module when operations are in use
+    .open = device_open,        // Points to the method to call when opening the device
+    .release = device_close,    // Points to the method to call when closing the device
+    .write = device_write,      // Points to the method to call when writing to the device
+    .read = device_read         // Points to the method to call when reading from the device
+};
+
+// int driver_entry(void)
 static int driver_entry(void)
 {
     // 3. Register our device with the system
@@ -43,7 +113,9 @@ static int driver_entry(void)
     }
     major_number = MAJOR(dev_num);  // Macro extracts the major number
     printk(KERN_INFO "myTestDevice: major number is %d\n", major_number);
-    printk(KERN_INFO "\tuse \"mknod /dev/%s c %d 0\" for device file", DEVICE_NAME, major_number);
+    // printk(KERN_INFO "\tDEVICE_NAME: %s\n", DEVICE_NAME);
+    // printk(KERN_INFO "\tMAJOR NUMBER: %d\n", major_number);
+    printk(KERN_INFO "\tUse 'mknod /dev/%s c %d 0' for device file\n", DEVICE_NAME, major_number);
 
     // 3.2. Allocate and initialize the character device structure
     myCdev = cdev_alloc();  // Create our cdev structure
@@ -71,21 +143,32 @@ static int driver_entry(void)
     else
     {
         // 4. Initialize the semaphore
-        retVal = sema_init(&virtual_device.sem, USYNC_THREAD, NULL);
+        // PRO TIP:  USYNC_THREAD and USYNC_PROCESS come from synch.h on Solaris
+        // retVal = sema_init(&virtual_device.sem, USYNC_THREAD, NULL);
+        // retVal = sema_init(&virtual_device.sem, USYNC_PROCESS, NULL);
+        // retVal = sema_init(&virtual_device.sem, 1, NULL);  // ERROR: TOO MANY ARGUMENTS
+        sema_init(&virtual_device.sem, 1);
 
-        if (retVal != 0)
-        {
-            printk(KERN_ALERT "myTestDevice: unable to initialize a semaphore\n");
-            return retVal;
-        }
+        // PRO TIP:  Non-Solaris sema_init has a return value of void
+        //     if (retVal != 0)
+        //     {
+        //         printk(KERN_ALERT "myTestDevice: unable to initialize a semaphore\n");
+        //         return retVal;
+        //     }
+        // }
     }
 
     return 0;
 }
 
 
+// static int driver_exit(void)
 static void driver_exit(void)
+// void driver_exit(void)
+// static exitcall_t driver_exit(void)
 {
+    // exitcall_t retVal = 0;
+
     // 5. Do everything in the reverse order
     // 5.1. Unregister the cdev
     cdev_del(myCdev);
@@ -95,6 +178,8 @@ static void driver_exit(void)
     printk(KERN_ALERT "myTestDevice: unloaded module\n");
 
     return;
+    // return 0;
+    // return retVal;
 }
 
 module_init(driver_entry);

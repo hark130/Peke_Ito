7fcd6bfddca1ffa97027f4ad802187cb1e3abcf9
hark130 <hark130@yahoo.com>
2017-09-24 07:57:59 -0700

    BLINK: Added filesystem read functionality
    Hopefully, this will allow user mode to communicate with my driver. -Hark

--- Modules/blinkDriver/blinkDeviceDriver.c
+++ Modules/blinkDriver/blinkDeviceDriver.c
@@ -12,6 +12,8 @@
 #define PRODUCT_ID  0x01ED
 #define BUFF_SIZE 8
 
+int device_open(struct inode *inode, struct file *filp);
+int device_close(struct inode *inode, struct file *filp);
 static int blink_probe(struct usb_interface* interface, const struct usb_device_id* id);
 static void blink_disconnect(struct usb_interface* interface);
 
@@ -47,7 +49,18 @@ static struct usb_driver blink_driver =
 // 4. USB Request Block pointer
 struct urb* blinkURB = NULL;
 
-// 5. Other
+// 5. Define file operations
+// This tells the kernel which functions to call when a user operates on our device file_operations
+struct file_operations blinkOps =
+{
+    .owner = THIS_MODULE,       // Prevent unloading of this module when operations are in use
+    .open = device_open,        // Points to the method to call when opening the device
+    .release = device_close,    // Points to the method to call when closing the device
+    .write = device_write,      // Points to the method to call when writing to the device
+    // .read = device_read         // Points to the method to call when reading from the device
+};
+
+// 6. Other
 int retVal;             // Will be used to hold return values of functions; this is because the kernel stack is very small
                         //  so declaring variables all over the place in our module functions eats up the stack very fast
 
@@ -55,6 +68,53 @@ int retVal;             // Will be used to hold return values of functions; this
 /////////////////////////////////////
 /* USB DEVICE DRIVER FUNCTIONALITY */
 /////////////////////////////////////
+
+/* FILE OPERATIONS */
+//  Called on defice_file open
+//      inode reference to the file on disk
+//      and contains information about that file
+//      struct file represents an abstract open file
+int device_open(struct inode *inode, struct file *filp)
+{
+    // Only allow one process to open this device by using a semaphore as mutal exclusive lock - mutext
+    retVal = down_interruptible(&virtual_device.sem);
+    if (retVal != 0)
+    {
+        printk(KERN_ALERT "%s: could not lock device during open\n", DEVICE_NAME);
+        return -1;
+    }
+    else
+    {
+        printk(KERN_INFO "%s: opened device\n", DEVICE_NAME);
+    }
+
+    return 0;
+}
+
+
+// Called upon user close
+int device_close(struct inode *inode, struct file *filp)
+{
+    // By calling up, which is opposite of down for semaphores, we release
+    //  the mutex that we obtained at device open
+    // This has the effect of allowing other processes to use the device now
+    up(&virtual_device.sem);
+    printk(KERN_INFO "%s: closed device\n", DEVICE_NAME);
+    return 0;
+}
+
+
+// Called when user wants to send information to the device
+ssize_t blink_write(struct file* filp, const char* bufSourceData, size_t bufCount, loff_t* curOffset)
+{
+    // Send data from user to kernel
+    // copy_from_user(dest, source, count)
+    printk(KERN_INFO "%s: writing to device\n", DEVICE_NAME);
+    return copy_from_user(virtual_device.data, bufSourceData, BUFF_SIZE);
+}
+
+
+/* DEVICE OPERATIONS */
 static int blink_probe(struct usb_interface* interface, const struct usb_device_id* id)
 {
     printk(KERN_INFO "%s: blink(1) (%04X:%04X) active\n", DEVICE_NAME, id->idVendor, id->idProduct);
@@ -69,16 +129,6 @@ static void blink_disconnect(struct usb_interface* interface)
 }
 
 
-// Called when user wants to send information to the device
-ssize_t blink_write(struct file* filp, const char* bufSourceData, size_t bufCount, loff_t* curOffset)
-{
-    // Send data from user to kernel
-    // copy_from_user(dest, source, count)
-    printk(KERN_INFO "%s: writing to device\n", DEVICE_NAME);
-    return copy_from_user(virtual_device.data, bufSourceData, BUFF_SIZE);
-}
-
-
 static int __init driver_entry(void)
 {
     // 1. Initialize the semaphore
@@ -90,6 +140,9 @@ static int __init driver_entry(void)
     {
         printk(KERN_ALERT "%s: Unable to register USB\nusb_register() returned %d\n", DEVICE_NAME, retVal);
     }
+
+    // 3. Allocate an URB to use
+    blinkURB = usb_alloc_urb(0, 0);
 
     return retVal;
 }

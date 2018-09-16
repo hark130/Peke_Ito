9a1f18d1acef666118aacfccf9b202d5138b48fa
hark130 <hark130@yahoo.com>
2018-09-16 17:27:06 -0500

    block1: block device driver incremental commit
    Still working on getting register_blkdev() to work.  Getting an error (EBUSY?).  Do I need modprobe instead of insmod?  I can't recall but I feel like I've already battled this and won.  Searching through the repo, it's polluted with this kernel.  Why is it here anyway?  Time to clean up this repo.  First, I'm saving my work.
    
    Changes to be committed:
    	modified:   myBlockDeviceDriver.c

--- Modules/myBlockDevice/myBlockDeviceDriver.c
+++ Modules/myBlockDevice/myBlockDeviceDriver.c
@@ -1,22 +1,27 @@
 /*
- *	PURPOSE - A block device driver for a virtual block device of my own creation
+ *    PURPOSE - A block device driver for a virtual block device of my own creation
  */
 
 /////////////
 /* HEADERS */
 /////////////
-#include <linux/module.h>       				// ALWAYS NEED
-#include <linux/kernel.h> 						// ALWAYS NEED
+#include <linux/fs.h>                           // Defines file table structures
+#include <linux/kernel.h>                       // ALWAYS NEED
+#include <linux/module.h>                       // ALWAYS NEED
 
 ////////////
 /* MACROS */
 ////////////
-#define DEVICE_NAME "Virtual Block Device 0"	// virtBlockDev0
+#define DEVICE_NAME "Virtual Block Device 0"    // virtBlockDev0
+#define DEV_MAJOR_NUM 1337                      // Major number
+#define DEV_MINOR_NUM 31337                     // Minor number
+#define HARKLE_KERROR(module, funcName, errNum) do { printk(KERN_ERR "%s: <<<ERROR>>> %s() returned %d!\n", #module, #funcName, errNum); } while (0);
 
 /////////////
 /* GLOBALS */
 /////////////
-int retVal;                          	      	// Will be used to hold return values of functions
+int retVal;                                     // Will be used to hold return values of functions
+int major_number;                               // Will store our major number - returned by register_blkdev()
 
 /////////////////////////
 /* FUNCTION PROTOTYPES */
@@ -29,10 +34,20 @@ static void __exit block_driver_exit(void);
 //////////////////////////
 static int __init block_driver_entry(void)
 {
-	retVal = 0;  // Placeholder for necessary function calls
+    // Register a new block device
+    // retVal = register_blkdev(0, DEVICE_NAME);
+    retVal = register_blkdev(DEV_MAJOR_NUM, DEVICE_NAME);
 
-    // Log it
-    printk(KERN_INFO "%s: loaded module\n", DEVICE_NAME);
+    if (retVal < 0)
+    {
+        HARKLE_KERROR(DEVICE_NAME, register_blkdev, retVal);
+    }
+    else
+    {
+        printk(KERN_INFO "%s: loaded module with major number %d\n", DEVICE_NAME, retVal);
+    }    
+
+    major_number = retVal;
 
     return retVal;
 }
@@ -40,7 +55,7 @@ static int __init block_driver_entry(void)
 
 static void __exit block_driver_exit(void)
 {
-    // Log it
+    unregister_blkdev(DEV_MAJOR_NUM, DEVICE_NAME);
     printk(KERN_INFO "%s: unloaded module\n", DEVICE_NAME);
 
     return;

